d5c260a6c05f7ef4b8dc87f5518074788fc4ac00
hernancham <hachambillac@unjbg.edu.pe>
2023-05-14 21:44:12 -0500

    correcion pepito rana

--- Maraton SurPeru Mayo23/A_pepito_rana.cpp
+++ Maraton SurPeru Mayo23/A_pepito_rana.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int menor_inmediato(int *array, int n, int pos_actual, int valor)
 {
     int pos = pos_actual;
-    if (array[n - 1] >= valor)
+    if (valor > array[n - 1])
         return n;
     for (int i = pos_actual; i < n; i++)
         if (array[i] <= valor)
@@ -19,29 +19,26 @@ int main()
     int n;
     while (cin >> n && n != 0)
     {
-        int pos = 0;
         int *piedras = new int[n];
         vector<int> saltos;
 
         for (int i = 0; i < n; i++)
             cin >> piedras[i];
 
-        while (pos < n)
+        int pos_act = 0, pos_sig;
+
+        while (pos_act < n)
         {
-            int pos_menor = menor_inmediato(piedras, n, pos, 2 * piedras[pos]);
-            if (pos_menor == pos)
-            {
-                cout << "Imposible para Pepito" << endl;
+            pos_sig = menor_inmediato(piedras, n, pos_act, 2 * piedras[pos_act]);
+            if (pos_sig == pos_act)
                 break;
-            }
-            else
-            {
-                saltos.push_back(piedras[pos_menor]);
-                pos = pos_menor;
-            }
+            saltos.push_back(piedras[pos_act]);
+            pos_act = pos_sig;
         }
 
-        if (pos == n)
+        if (pos_act < n)
+            cout << "Imposible para Pepito" << endl;
+        else
             for (auto elem : saltos)
                 cout << elem << " ";
 
